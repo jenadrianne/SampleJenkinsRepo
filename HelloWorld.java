f3418a5eb2cfad09e38b82bd39e30ef607f2936c
ja.caballes <ja.caballes@fujitsu.com>
2023-09-19 11:22:27 +0800

    test main

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,5 +4,7 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
 		System.out.println("Hello, Earth!"); 
+		System.out.println("Hello, World!"); 
+		System.out.println("Hello, Earth!"); 
     }
 }
